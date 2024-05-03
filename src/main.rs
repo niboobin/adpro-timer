25d6d9fb1f4bd842a425540ef51181621be53dca
Obin Mandalika <obinnmandalikaa@gmail.com>
2024-05-03 13:35:42 +0700

    “Experiment 1.2: Understanding how it works.

--- src/main.rs
+++ src/main.rs
@@ -104,6 +104,7 @@ fn main() {
         println!("Obin's Computer: done!");
     });
 
+    println!("Obin's Computer: hey hey");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
     drop(spawner);
